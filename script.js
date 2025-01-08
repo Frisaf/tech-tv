71f57974aec7f862d6d81303fe3ff0c1ad9e975c
Frisaf <fridasafsten@gmail.com>
2025-01-08 15:22:04 +0100

    mobile nav

--- script.js
+++ script.js
@@ -12,3 +12,26 @@ for (i = 0; i < dropdown.length; i++) {
         }
     });
 }
+
+function openNav() {
+    document.getElementById("sidePanel").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("sidePanel").style.width = "0";
+}
+
+var dropdown = document.getElementsByClassName("sidePanelDropdownBtn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+    dropdown[i].addEventListener("click", function() {
+        this.classList.toggle("activeDropdown");
+        var dropdownContent = this.nextElementSibling;
+        if (dropdownContent.style.display === "block") {
+            dropdownContent.style.display = "none";
+        } else {
+            dropdownContent.style.display = "block"
+        }
+    });
+}
