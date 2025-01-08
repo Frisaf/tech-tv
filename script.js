0ea99d81fe7b44a543c04728198b192420ce5ffc
Frisaf <frisaf07@gmail.com>
2025-01-08 21:11:07 +0100

    continue on mobile nav

--- script.js
+++ script.js
@@ -14,24 +14,9 @@ for (i = 0; i < dropdown.length; i++) {
 }
 
 function openNav() {
-    document.getElementById("sidePanel").style.width = "250px";
+    document.getElementById("sidePanel").style.width = "500px";
 }
 
 function closeNav() {
     document.getElementById("sidePanel").style.width = "0";
-}
-
-var dropdown = document.getElementsByClassName("sidePanelDropdownBtn");
-var i;
-
-for (i = 0; i < dropdown.length; i++) {
-    dropdown[i].addEventListener("click", function() {
-        this.classList.toggle("activeDropdown");
-        var dropdownContent = this.nextElementSibling;
-        if (dropdownContent.style.display === "block") {
-            dropdownContent.style.display = "none";
-        } else {
-            dropdownContent.style.display = "block"
-        }
-    });
 }
