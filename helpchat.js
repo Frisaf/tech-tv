ebf0a0a4a5066cd3eef484971723edfaeaa34c1f
Frisaf <fridasafsten@gmail.com>
2025-02-25 15:14:15 +0100

    helpchat funcitons complete

--- helpchat.js
+++ helpchat.js
@@ -1,8 +1,27 @@
-function showChat() {
+const showChatButton = document.querySelector(".helpChat")
+
+showChatButton.addEventListener("click", (event) => {
+    console.log("click")
     var chat = document.getElementById("chatContent");
-    if (chat.style.display === "none") {
-        chat.style.display = "block";
-    } else {
-        chat.style.display = "none";
-    }
+    chat.classList.toggle("hidden")
+})
+
+var messageElement = document.getElementById("message");
+
+function sendMessage() {
+    var userMessage = document.getElementById("userMessage");
+
+    document.getElementById("userMessage").innerText = "Du: " + messageElement.value;
+    userMessage.style.display = "block";
+    messageElement.value = "";
+
+    setTimeout(showStaffMessage, 1000);
+}
+
+function showStaffMessage() {
+    var staffMessage = document.getElementById("staffMessage");
+
+    document.getElementById("staffMessage").innerText = "Support: Hej! Välkommen till vår kundsupport. Just nu är det många som vill ha hjälp. Din plats i kön är: 3674378534985793485. Tack för att du väntar! Din tid betyder mycket för oss.";
+    staffMessage.style.display = "block";
+    console.log(messageElement)
 }
